132f50b7a47d351ccddf1993e86a2465dab5fe70
Christopher Terrano <mterrano1@yahoo.com>
2022-03-28 13:55:11 -0500

    commit tree dba5b19fa59a53554b841ff4703524f09144fff7

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,38 @@
 class Triangle
   # write code here
+  attr_accessor :sideOne, :sideTwo, :sideThree
+
+  def initialize sideOne, sideTwo, sideThree
+    @sideOne = sideOne
+    @sideTwo = sideTwo
+    @sideThree = sideThree
+  end
+
+  def kind
+    validate_triangle
+    if sideOne == sideTwo && sideTwo == sideThree
+      :equilateral
+    elsif sideOne == sideTwo || sideTwo == sideThree || sideOne == sideThree
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+    private
+
+  def greater_than_zero
+    [sideOne, sideTwo, sideThree].all?(&:positive?)
+  end
+
+  def triangle_inequality
+    sideOne + sideTwo > sideThree && sideOne + sideThree > sideTwo && sideTwo + sideThree > sideOne
+  end
+
+  def validate_triangle
+    raise TriangleError unless greater_than_zero && triangle_inequality
+  end
+
+  class TriangleError < StandardError
+  end
 end
